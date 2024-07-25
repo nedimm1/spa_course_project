44d2b3652d1ff21606676c6eacb9e7e092e237b5
github_username <nedimmuminovic61@gmail.com>
2024-07-25 19:29:15 +0200

    using the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
   return (
     <div>
-      <h1>WorldWise</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
